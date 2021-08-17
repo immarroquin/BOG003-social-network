be3cdb0e05055e0ecc9dbbbf8e1a0569b4e0c7d8
Lorena Marroquin <lorenamarroquin56@gmail.com>
2021-08-17 18:00:27 -0500

    Escribimos los respectivos script de Fb en html, utilizamos firebase auth para registro con correo y contraseña

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,17 +1,2 @@
 // aqui exportaras las funciones que necesites
 
-export const register = () => {
-  const email = 'test@example.com';
-  const password = 'hunter2';
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
-};
