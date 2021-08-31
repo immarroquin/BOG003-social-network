89c7b79843b9643dc04000fb5958b76ea726adf0
immarroquin <lorenamarroquin56@gmail.com>
2021-08-31 09:40:46 -0500

    Estamos realizando las pruebas unitarias

--- src/lib/index.js
+++ src/lib/index.js
@@ -45,8 +45,7 @@ export const signOut = () => {
 
 export const loginGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth()
-    .signInWithPopup(provider)
+  firebase.auth().signInWithPopup(provider)
     .then(() => {
       window.location.href = '#/home';
     });
