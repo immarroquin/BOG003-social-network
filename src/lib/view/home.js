c9651560cebf86ad370cd04773c706fdc761414c
immarroquin <lorenamarroquin56@gmail.com>
2021-09-03 11:05:56 -0500

    bajamos cambios de dev-Diana

--- src/lib/view/home.js
+++ src/lib/view/home.js
@@ -5,7 +5,7 @@ import {
   deletePost,
 } from '../index.js';
 // eslint-disable-next-line
-import { defaultApp } from '../configfirebase';
+import { defaultApp } from '../configfirebase.js';
 
 export const home = () => {
   const divHome = document.createElement('div');
