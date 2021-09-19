2c117bf221e2aebb557e297e784fa29269bfadd8
Sergio Sinuco <ssinuco@gmail.com>
2021-09-19 17:38:35 -0500

    Added new route #/add to show the popup for creating a post.

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import {
   login,
 } from './lib/view/login.js';
 import {
-  changeRoute,
+  showTemplate,
 } from './lib/router.js';
 
 const init = () => {
   document.getElementById('container').appendChild(login());
-  changeRoute(window.location.hash);
+  showTemplate(window.location.hash);
   window.addEventListener('hashchange', () => {
-    changeRoute(window.location.hash);
+    showTemplate(window.location.hash);
   });
 };
 window.addEventListener('load', init);
